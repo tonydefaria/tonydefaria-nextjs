f4ca1c800b7b08820413a05cc8a300aeefe80e27
Tony de Faria <me@tonydefaria.com>
2022-08-15 21:36:27 +0200

    add custom headers

--- next.config.js
+++ next.config.js
@@ -1,14 +1,30 @@
 /** @type {import("next").NextConfig} **/
+
 const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   experimental: {
     images: {
-      allowFutureImage: true
+      allowFutureImage: true,
+      formats: ["image/avif", "image/webp"],
     }
   },
   images: {
     domains: ["hankyo-production.fra1.cdn.digitaloceanspaces.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|avif|webp)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate",
+          }
+        ],
+      },
+    ]
   },
 }
 
