1ac5e8882a6639262feacced849dda2c5cf9d40e
Tony de Faria <me@tonydefaria.com>
2022-08-17 00:13:55 +0200

    a lot going on here

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Index({sectionData}) {
 
   return (
     <Layout>
+
       {/* Hero */}
       <div className="intro">
         <div className="intro-box">
@@ -30,6 +31,57 @@ export default function Index({sectionData}) {
             </figure>
           </div>
         </div>
+      </div>
+
+      <h1>Excepteur sint occaecat</h1>
+      <hr className="separator-xl" />
+      <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+      <hr className="separator-xl" />
+      <a href="#" className="button-m">I am a link</a>
+      <hr className="separator-xxl" />
+      <div className="writer">
+        <h1>Excepteur sint occaecat</h1>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+
+        <h2>Excepteur sint occaecat</h2>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+
+        <h3>Excepteur sint occaecat</h3>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+
+        <h4>Excepteur sint occaecat</h4>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+
+        <h5>Excepteur sint occaecat</h5>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+
+        <h6>Excepteur sint occaecat</h6>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+
+        <ol>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+        </ol>
+
+        <ul>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+          <li>Nulla facilisi <a href="#">nullam</a> vehicula ipsum</li>
+        </ul>
+
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
+        <p>Ultricies lacus sed turpis tincidunt. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Leo in vitae turpis massa sed elementum. Nullam eget felis <a href="#">eget</a> nunc lobortis mattis. Enim ut tellus elementum sagittis vitae et leo. Vel eros donec ac odio tempor. Ultrices dui sapien eget mi proin sed. Enim ut sem viverra aliquet eget sit amet tellus cras.</p>
       </div>
     </Layout>
   )
